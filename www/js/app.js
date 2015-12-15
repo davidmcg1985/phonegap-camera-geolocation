2cc2fe9ae6c42b982a3502ede490d7a518c9a4cd
David McGowan <dmcgweb101@gmail.com>
2015-12-15 20:40:08 +0000

    added compass functionality and css styling

--- www/js/app.js
+++ www/js/app.js
@@ -1,76 +1,151 @@
-// $(document).ready(function() {
-	
-// 	$("#takePhoto").click(function() {
+// global variables 
+var watchID;
 
-// 		navigator.camera.getPicture(onSuccess, onFail, { 
-// 			quality: 100,
-// 		    destinationType: Camera.DestinationType.DATA_URL,
-// 		    allowEdit : true,
-// 		    saveToPhotoAlbum: true
-// 		});
+document.addEventListener("deviceready", onDeviceReady, false);
 
-// 		function onSuccess(imageData) {
-// 		    var image = document.getElementById('myImage');
-// 		    image.src = "data:image/jpeg;base64," + imageData;
-// 		}
+function onDeviceReady() {
 
-// 		function onFail(message) {
-// 		    alert('Failed because: ' + message);
-// 		}
-// 	});
+	console.log("Entering onDeviceReady");
+    console.log("Cordova: " + device.cordova);
 
-// });
+    //Setup the watch
+	//Read the compass every second (500 milliseconds)
+	var watchOptions = {
+		frequency: 500,
+	};
+
+	console.log('Creating watch: %s', JSON.stringify(watchOptions));
+  	watchID = navigator.compass.watchHeading(onSuccess, onError, watchOptions);
+
+
+	$('#items').on('click', '.delete', function (e) {
+		itemList.deleteItem(parseInt($(this).parent().find('.key').text()));
+		return false;
+	});
+		
+	itemList.open();
+
+    if( $('#items li').length > 0 ) {
+		console.log('there is items in the list');
+		$('#itemHeading').text('Your recent photos');
+	} else {
+		console.log('no items in list');
+		$('#itemHeading').text('Nothing to show, why dont you take a photo?');
+	}
+
+  	console.log("Leaving onDeviceReady");
+};
+
+
+// compass
+function onSuccess(heading) {
+  console.log("Entering onSuccess");
+  console.log('Received Heading');
+  console.log(JSON.stringify(heading));
+  var hv = Math.round(heading.magneticHeading);
+  $('#headingInfo').html('Heading: ' + hv + ' &deg;');
+  console.log('Rotating to ' + hv + ' &deg;');
+  $("#compassImg").rotate(-hv);
+  console.log("Leaving onSuccess");
+}
+
+function onError(err) {
+  var msg;
+  console.log("Entering onError");
+  console.error('Error: ' + JSON.stringify(err));
+  //Remove the watch since we're having a problem
+  navigator.compass.clearWatch(watchID);
+  //Clear the heading value from the page  
+  $('#headingInfo').html('<b>Heading: </b>None');
+  //Then tell the user what happened.
+  switch (err.code) {
+  case CompassError.COMPASS_NOT_SUPPORTED:
+    msg = 'Compass not supported';
+    break;
+  case CompassError.COMPASS_INTERNAL_ERR:
+    msg = 'Internal compass error';
+    break;
+  default:
+    msg = 'Unknown heading error!';
+  }
+  console.error(msg);
+  navigator.notification.alert(msg, null, "Compass Error", "Continue");
+  console.log("Leaving onError");
+}
+
 
 // geolocation and localstorage
-
 // bind button and ul
 $(function () {
-	$('[type=submit]').on('click', function (e) {
-		photoList.addPhoto(
-			$('#name').val(),
-			'',
-			'',
-			'',
-			'',
-			'',
-			''
-		);
-		$('input:text').val('');
-		return false;
+	$('#submit').on('click', function (e) {
+
+		navigator.camera.getPicture(onPhotoDataSuccess, onFail, { 
+			quality: 50,
+			destinationType: Camera.DestinationType.DATA_URL,
+			allowEdit: true,
+			saveToPhotoAlbum: true
+		});
+
+		function onPhotoDataSuccess(imageData) {
+			var myImage = document.getElementById('myImage');
+			myImage.src = "data:image/jpeg;base64," + imageData;
+			console.log(myImage.src);
+
+			itemList.addItem(
+				myImage.src,
+				$('#name').val(),
+				'',
+				'',
+				$('#headingInfo').html(),
+				'',
+				'',
+				''
+			);
+			$('input:text').val('');
+			$('#itemHeading').text('Your recent photos');
+			return false;			
+		}
+
+		$('#items').on('click', '.delete', function (e) {
+			itemList.deleteItem(parseInt($(this).parent().find('.key').text()));
+			return false;
+		});
+		
+		itemList.open();
+
+		function onFail(message) {
+			console.log('Failed because: ' + message);
+		}
 	});
-	$('#photos').on('click', '.delete', function (e) {
-		photoList.deletePhoto(parseInt($(this).parent().find('.key').text()));
-		return false;
-	});
-	photoList.open();
 });
 
-photoList = {}; 
+itemList = {}; 
 
-photoList.open = function() {
+itemList.open = function() {
 	this.list = { }; 
-	if (localStorage.photoList) {
-		 this.list = JSON.parse(localStorage.photoList);
-	} 
-	photoList.getAllPhotos();
+	if (localStorage.itemList) {
+		 this.list = JSON.parse(localStorage.itemList);
+	}
+	itemList.getAllItems();
 };
 
 // add - method renamed and more arguments
-photoList.addPhoto = function(name,lat,lng,heading,speed,altitude,timestamp) {
+itemList.addItem = function(photo,name,lat,lng,headingInfo,speed,altitude,timestamp) {
 	console.log(arguments.callee.name, arguments);
 	key = new Date().getTime();
 	this.list[key] = {
+		'photo':photo,
 		'name':name,
 		'lat':lat,
 		'lng':lng,
-		'heading':heading,
+		'headingInfo':headingInfo,
 		'speed':speed,
 		'altitude':altitude,
 		'timestamp':timestamp
 		};
-	localStorage.photoList = JSON.stringify(this.list);
+	localStorage.itemList = JSON.stringify(this.list);
 	
-	this.getAllPhotos();
+	this.getAllItems();
 
 	if (navigator.geolocation) {
 
@@ -91,38 +166,41 @@ photoList.addPhoto = function(name,lat,lng,heading,speed,altitude,timestamp) {
 };
 
 // read each item from list and render on display
-photoList.getAllPhotos = function() {
-	$('#photos').html('');
+itemList.getAllItems = function() {
+	$('#items').html('');
 	for (var key in this.list) {
-		renderPhoto(key, this.list[key]);
+		renderItem(key, this.list[key]);
 	}
 };
 
 // delete
-photoList.deletePhoto = function(key) { 
+itemList.deleteItem = function(key) { 
 	console.log(arguments.callee.name, arguments);
 	delete this.list[key];
-	localStorage.photoList = JSON.stringify(this.list);
-	this.getAllPhotos(); 
+	localStorage.itemList = JSON.stringify(this.list);
+	this.getAllItems(); 
+	if ($('#items li').length == 0) {
+		$('#itemHeading').text('Nothing to show, why dont you take a photo?');
+	}
 };
 
 
 // update - method has more arguments
-photoList.updatePosition = function(lat,lng,heading,speed,altitude,timestamp,key) {
+itemList.updatePosition = function(lat,lng,speed,altitude,timestamp,key) {
 	console.log(arguments); 
 	this.list[key]['lat'] = lat;
 	this.list[key]['lng'] = lng;
-	this.list[key]['heading'] = heading;
+	// this.list[key]['heading'] = heading;
 	this.list[key]['speed'] = speed;
 	this.list[key]['altitude'] = altitude;
 	this.list[key]['timestamp'] = timestamp;
-	localStorage.photoList = JSON.stringify(this.list); 
-	this.getAllPhotos();  
+	localStorage.itemList = JSON.stringify(this.list); 
+	this.getAllItems();  
 };
 	
 
-// render output to #photos ul
-function renderPhoto(key,value) {
+// render output to #items ul
+function renderItem(key,value,timestamp) {
 
 	var today = new Date(value.timestamp);
 	var dd = today.getDate();
@@ -131,27 +209,29 @@ function renderPhoto(key,value) {
 	var yyyy = today.getFullYear();
 	if(dd<10){dd='0'+dd}
 	if(mm<10){mm='0'+mm}
-	var today = mm+'/'+dd+'/'+yyyy;
+	var today = dd+'/'+mm+'/'+yyyy;
 
-	var li = '<li>Name: <span class="name">'+value.name+'</span><br/>';
-	li += 'Latitude: <span class="lat">'+value.lat+'</span><br/>';
-	li += 'Longitude: <span class="lng">'+value.lng+'</span><br/>';
-	li += 'Heading: <span class="heading">'+value.heading+' Degrees</span><br/>';
-	li += 'Speed: <span class="speed">'+value.speed+' mps</span><br/>';
-	li += 'Altitude: <span class="altitude">'+value.altitude+' metres</span><br/>';
-	li += 'Date: <span class="timestamp">'+ today +'</span><br/>';
-	li += '<a href="#" class="delete">Delete</a><span class="key">'+key+'</span></li>';
-	$('#photos').append(li);
+	var li = '<li><span class="photo"><img src="'+ value.photo +'" style="width: 100%; border-bottom: #38C 2px solid;"/></span><br/>';
+	li += '<i class="fa fa-user"></i>&nbsp;<span class="name">'+value.name+'</span><br/>';
+	li += '<i class="fa fa-map-marker"></i>&nbsp;<span class="lat">'+value.lat+' (Lat)</span><br/>';
+	li += '<i class="fa fa-map-marker"></i>&nbsp;<span class="lng">'+value.lng+' (Lng)</span><br/>';
+	li += '<i class="fa fa-compass"></i>&nbsp;<span class="heading">'+value.headingInfo+'</span><br/>';
+	li += '<i class="fa fa-tachometer"></i>&nbsp;<span class="speed">'+value.speed+' mps</span><br/>';
+	li += '<i class="fa fa-area-chart"></i>&nbsp;<span class="altitude">'+value.altitude+' metres</span><br/>';
+	li += '<i class="fa fa-calendar"></i>&nbsp;<span class="timestamp">'+ today +'</span><br/>';
+	li += '<a href="#" class="delete ui-btn">Delete</a><span class="key">'+key+'</span></li>';
+	$('#items').prepend(li);
+
 }
 
 // update coords
 function updatePosition(position, key) {
 	console.table(position); // postion Object
-	console.log(position.coords.latitude, position.coords.longitude, position.coords.heading, position.coords.speed, position.coords.altitude, position.timestamp);
+	console.log(position.coords.latitude, position.coords.longitude, position.coords.speed, position.coords.altitude, position.timestamp);
 	console.log(key); // when GPS called and record to be updated
 	console.table(new Date().getTime()); // when GPS returned
 	console.log(position.timestamp);
-	photoList.updatePosition(position.coords.latitude, position.coords.longitude, position.coords.heading, position.coords.speed, position.coords.altitude, position.timestamp, key);
+	itemList.updatePosition(position.coords.latitude, position.coords.longitude, position.coords.speed, position.coords.altitude, position.timestamp, key);
 }
 
 // error handling
